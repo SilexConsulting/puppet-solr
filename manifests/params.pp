0ca82c55fe4e244d92142f3aa1a4d48df74b73c0
Jason Brown <jason@silex-consulting.com>
2014-08-26 23:09:57 +0100

    Update solr::params to use parameterised tomcat version.

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,19 +4,22 @@
 # It sets variables according to platform
 #
 class solr::params {
+  $tomcat_version = 6
+  $tomcat_home                        = "/usr/share/tomcat${tomcat_version}"
+  $tomcat_basedir                     = "/var/lib/tomcat${tomcat_version}"
+  $tomcat_user                        = "tomcat"
+  $tomcat_group                       = "tomcat"
+
+  $solr_home                          = '/usr/share/solr'
+  $solr_conf_dir                      = '/etc/solr'
+  $solr_version                       = '4.6.0'
+  $service_name                       = "tomcat${tomcat_version}"
+
+  $config                             = 'puppet:///modules/solr/'
   case $::osfamily {
     'Debian': {
-      $tomcat_home                        = '/usr/share/tomcat7'
-      $tomcat_basedir                     = '/var/lib/tomcat7'
-      $tomcat_user                        = 'tomcat7'
-      $tomcat_group                       = 'tomcat7'
-
-      $solr_home                          = '/usr/share/solr'
-      $solr_conf_dir                      = '/etc/solr'
-      $solr_version                       = '4.6.0'
-      $service_name                       = 'tomcat7'
-
-      $config                             = 'puppet:///modules/solr/'
+    }
+    'RedHat': {
     }
     default: {
       fail("${::operatingsystem} not supported")
